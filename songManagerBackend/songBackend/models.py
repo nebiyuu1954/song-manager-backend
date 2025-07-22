8035cec1508fa758932c011f9c63534647f722f4
Nebiyu195494 <107809704+nebiyuu1954@users.noreply.github.com>
2025-07-22 18:47:59 +0300

    added the model for song database

--- songManagerBackend/songBackend/models.py
+++ songManagerBackend/songBackend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    year = models.IntegerField()
+    stream = models.IntegerField()
+
+    def __str__(self):
+        return self.title
